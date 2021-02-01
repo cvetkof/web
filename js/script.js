fa108741eb436828f65b94316bf88a2401644b6c
cvetkof <cvetkoffff15@mail.ru>
2021-02-01 22:23:15 +0300

    01.02.21

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('salam aleykum')
+$('.change-btn').click(function () {
+	$('.change-show').toggleClass("show");
+	$('.first').toggleClass("rotate");
+});
+
+$('.sidebar-btn').click(function () {
+	$(this).toggleClass("click");
+	$('.main-content').toggleClass("click-for-sidebar-btn");
+	$('.sidebar').toggleClass("show");
+});
