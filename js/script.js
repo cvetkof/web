d485f3748a8d7776c41839ee05b905c3d8a25035
cvetkof <cvetkoffff15@mail.ru>
2021-02-04 22:29:31 +0300

    04.02.2021

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-$('.change-btn').click(function () {
-	$('.change-show').toggleClass("show");
-	$('.first').toggleClass("rotate");
-});
+// $('.change-btn').click(function () {
+// 	$('.change-show').toggleClass("show");
+// 	$('.first').toggleClass("rotate");
+// });
 
-$('.sidebar-btn').click(function () {
-	$(this).toggleClass("click");
-	$('.main-content').toggleClass("click-for-sidebar-btn");
-	$('.sidebar').toggleClass("show");
-});
+// $('.sidebar-btn').click(function () {
+// 	$(this).toggleClass("click");
+// 	$('.main-content').toggleClass("click-for-sidebar-btn");
+// 	$('.sidebar').toggleClass("show");
+// });
+
+
+document.querySelector('.change-btn').onclick = () => {
+	document.querySelector('.change-show').classList.toggle("show");
+	document.querySelector('.first').classList.toggle("rotate");
+};
+
+document.querySelector('.sidebar-btn').onclick = () => {
+	document.querySelector('.sidebar-btn').classList.toggle("click");
+	document.querySelector('.main-content').classList.toggle("click-for-sidebar-btn");
+	document.querySelector('.sidebar').classList.toggle("show");
+};
