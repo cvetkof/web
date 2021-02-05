abf776a421921c75da00f044aa66bdac41478bf5
cvetkof <cvetkoffff@mail.ru>
2021-02-05 16:12:42 +0300

    05.02.2021-1

--- js/script.js
+++ js/script.js
@@ -20,3 +20,13 @@ document.querySelector('.sidebar-btn').onclick = () => {
 	document.querySelector('.main-content').classList.toggle("click-for-sidebar-btn");
 	document.querySelector('.sidebar').classList.toggle("show");
 };
+
+document.querySelector('.row-column-3-row').onmouseover = () => {
+	document.querySelector('.block-header-3').style.color = '#bce1f7';
+	document.querySelector('.row-column-txt-3').style.color = '#bce1f7';
+	document.querySelector('.row-column-3-row').onmouseout = () => {
+		document.querySelector('.block-header-3').style.color = 'white';
+	}
+};
+
+console.log(document.querySelectorAll('.row-column-3-row'));
